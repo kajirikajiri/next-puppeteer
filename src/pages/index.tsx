30e5f158a53015fd377cdec27513fbbf9031a798
kajirikajiri <kajirikajiri@gmail.com>
2021-01-23 08:23:48 +0900

    :art:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
 import React, {FC} from 'react'
 
 const Home: FC = () => {
-  console.log(process.env.PLAIN)
-  console.log(process.env.SECRET)
   console.log(process.env.SERVICE_ACCOUNT)
   console.log(JSON.parse(process.env.SERVICE_ACCOUNT))
   return (
