e4f2a8eb8ff8b9368c1bd9cd21032b97e54e46e9
kajirikajiri <kajirikajiri@gmail.com>
2021-01-23 08:44:50 +0900

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,9 @@ import React, {FC} from 'react'
 
 const Home: FC = () => {
   console.log(1, process.env.SERVICE_ACCOUNT)
-  console.log(2, JSON.parse(process.env.SERVICE_ACCOUNT))
   console.log(3, process.env.private_key)
+  let a = JSON.parse(process.env.SERVICE_ACCOUNT)
+  a.private_key = process.env.private_key
   return (
     <div>hello</div>
   )
