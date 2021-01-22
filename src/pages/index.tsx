01570082e72cb67ef14a099ba5304c09a2493a28
kajirikajiri <kajirikajiri@gmail.com>
2021-01-23 07:58:15 +0900

    :art:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ const Home: FC = () => {
   console.log(process.env.PLAIN)
   console.log(process.env.SECRET)
   console.log(process.env.SERVICE_ACCOUNT)
+  console.log(JSON.parse(process.env.SERVICE_ACCOUNT))
   return (
     <div>hello</div>
   )
