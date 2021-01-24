bdb18bbe47b321cc5b223877d4d2039eba018925
kajirikajiri <kajirikajiri@gmail.com>
2021-01-24 16:46:29 +0900

    :sparkles:

--- src/presentation/pages/login/index.tsx
+++ src/presentation/pages/login/index.tsx
@@ -2,13 +2,14 @@ import firebase from "firebase";
 import React, {FC, useEffect, useState } from "react";
 import { getFirebaseConfig } from "src/scripts/getFirebaseConfig";
 import { sendMessage } from "src/scripts/sendMessage";
+import { useForm } from "react-hook-form";
 
 export const Login:FC = () => {
-  const [email, setEmail] = useState('hogehoge@example.com')
-  const [pass, setPass] = useState('password')
   const [loading, setLoading] = useState(true)
   const [isLogin, setIsLogin] = useState(undefined)
+  const [screen, setScreen] = useState<'login'|'signup'>('login')
   const [width, setWidth] = useState(0)
+  const { register, handleSubmit, errors, reset } = useForm();
   // Initialize Firebase
   if (!(firebase?.apps?.length > 0)) {
     try {
@@ -24,63 +25,121 @@ export const Login:FC = () => {
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         setIsLogin(true)
-        console.log(user);
-        ;(async()=>{
-          const a = await sendMessage({userUuid: user.uid})
-        })()
-        setTimeout(async()=>{
-          const a = await sendMessage({toggle: true})
-        }, 2000)
+        success(user)
       } else {
         setIsLogin(false)
-        console.log('please login')
       }
       setLoading(false)
     });
   }, [])
 
-  const handleClick=(type:'login'|'signup')=>{
-    if (type === 'login') {
-      firebase.auth().signInWithEmailAndPassword(email, pass)
-        .then(({user}) => {
-          console.log('login', user)
-          ;(async()=>{
-            const a = await sendMessage({userUuid: user.uid})
-          })()
-          setTimeout(async()=>{
-            const a = await sendMessage({toggle: true})
-          }, 2000)
-          })
-        .catch((error) => {
-        });
-    } else if(type==="signup") {
-      firebase.auth().createUserWithEmailAndPassword(email, pass)
-        .then((user) => {
-          console.log('signup')
-          console.log(user)
-        })
-        .catch((error) => {
-          console.log(error)
-        });
-      }
+  const onSubmitLogin = ({email, password}) => {
+    doLogin(email, password)
+  }
+  const onSubmitSignup = ({email, password}) => {
+    doSignup(email, password)
+  }
+  const doLogin =(email, password)=>{
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(({user}) => {
+        success(user)
+      })
+      .catch((error) => {
+        alert('ログインに失敗しました')
+      });
+  }
+  const doSignup = (email, password) => {
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then((user) => {
+        success(user)
+      })
+      .catch((error) => {
+        alert('登録に失敗しました')
+      });
+  }
+  const success = (user)=>{
+    ;(async()=>{
+      const a = await sendMessage({userUuid: user.uid})
+    })()
+    setTimeout(async()=>{
+      const a = await sendMessage({toggle: true})
+    }, 2000)
   }
   if (loading) {
     return (
-      <div>restore login ...</div>
+      <div className="container">... ログイン状態を復元しています。</div>
     )
   }
 
   if (isLogin === true) {
     return (
-      <div>now open menu !!</div>
+      <div className="container"><p>ログインしました。お待ち下さい</p></div>
     )
   }
 
-  if (isLogin === false) {
+  if (isLogin === false && screen === 'login') {
     return (
-      <button onClick={()=>handleClick("login")}>
-        buttona
-      </button>
+      <>
+        <div className="container">
+          <div className="login-form">
+            <form onSubmit={handleSubmit(onSubmitLogin)}>
+              <div className="form-group">
+                <label htmlFor="email">
+              <input id="email" type="text" name="email" placeholder="email" ref={register({ required: true, minLength: 4,       pattern: {
+        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+        message: "メールアドレスの形式が無効です"
+      } })} />
+                  {errors.email && <div className="error">メールアドレスを入力してください</div>}
+                </label>
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">
+                <input id="password" type="password" name="password" placeholder="password" ref={register({ required: true, minLength: 6 })} />
+                  {errors.password && <div className="error">パスワードを入力してください</div>}
+                </label>
+              </div>
+              <a style={{ cursor: 'pointer', marginRight: 40, color: 'grey', fontSize: 14 }} onClick={() => {
+                setScreen('signup')
+                reset()
+                }}>登録</a>
+              <button type="submit">ログイン</button>
+            </form>
+          </div>
+        </div>
+      </>
+    )
+  }
+
+  if (isLogin === false && screen === 'signup') {
+    return (
+      <>
+        <div className="container">
+          <div className="login-form">
+            <form onSubmit={handleSubmit(onSubmitSignup)}>
+              <div className="form-group">
+                <label htmlFor="email">
+            <input id="email" type="text" name="email" placeholder="email" ref={register({ required: true, minLength: 4, pattern: {
+        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+        message: "メールアドレスの形式が無効です"
+      }})} />
+                  {errors.email && <div className="error">メールアドレスを入力してください</div>}
+                </label>
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">
+              <input id="password" type="password" name="password"  placeholder="password" ref={register({ required: true, minLength: 6 })} />
+                  {errors.password && <div className="error">パスワードを入力してください</div>}
+                </label>
+              </div>
+              <a style={{ cursor: 'pointer', marginRight: 40, color: 'grey', fontSize: 14 }} onClick={() => {
+                setScreen('login')
+                reset()
+              }}>ログイン</a>
+              <button type="submit">登録</button>
+            </form>
+          </div>
+        </div>
+      </>
     )
   }
 }
