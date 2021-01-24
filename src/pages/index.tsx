403f4cc212794abb287f7d753d85cf5103b2fec0
kajirikajiri <kajirikajiri@gmail.com>
2021-01-24 09:42:12 +0900

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import React, {FC} from 'react'
 
 const Home: FC = () => {
   console.log('fireconf', process.env.FIREBASE_CONFIG)
+  console.log(JSON.parse(process.env.FIREBASE_CONFIG))
   return (
     <div>hello</div>
   )
