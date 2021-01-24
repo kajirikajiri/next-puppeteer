189bfc09975bfc2dd93b8d9500cc876d07f59be2
kajirikajiri <kajirikajiri@gmail.com>
2021-01-24 09:44:14 +0900

    fix

--- src/scripts/getFirebaseConfig/index.ts
+++ src/scripts/getFirebaseConfig/index.ts
@@ -1,6 +1,5 @@
 export const getFirebaseConfig = ()=>{
   if (process.env.NODE_ENV === 'production' && process.env.FIREBASE_CONFIG) {
-    console.log(process.env.FIREBASE_CONFIG)
     const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG)
     return firebaseConfig
   } else if (process.env.NODE_ENV === 'development') {
